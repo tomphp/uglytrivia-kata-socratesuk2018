7ad03bc8f5f69b8d1f7e948eba76dd177c290c18
Tom Oram <tom@codept.com>
2018-06-10 20:47:50 +0100

    Extract round logic into a GameEngine class

--- src/main/kotlin/com/adaptionsoft/games/uglytrivia/Game.kt
+++ src/main/kotlin/com/adaptionsoft/games/uglytrivia/Game.kt
@@ -3,14 +3,10 @@ package com.adaptionsoft.games.uglytrivia
 class Game {
     var players = Players()
 
-    private val popCategory = QuestionCategory("Pop")
-    private val scienceCategory = QuestionCategory("Science")
-    private val sportsCategory = QuestionCategory("Sports")
-    private val rockCategory = QuestionCategory("Rock")
+    private var play = Play(Roll(1))
+    private val gameEngine = GameEngine(players)
 
-    private var isGettingOutOfPenaltyBox: Boolean = false
-    private var roll = Roll(1)
-    private var answeredCorrectly = false
+    class Play(val roll: Roll, var answeredCorrectly: Boolean = false)
 
     fun add(playerName: String): Boolean {
         players.add(Player(playerName))
@@ -21,102 +17,19 @@ class Game {
     fun roll(roll: Int) = roll(Roll(roll))
 
     private fun roll(roll: Roll) {
-        this.roll = roll
+        this.play = Play(roll)
     }
 
     fun wasCorrectlyAnswered(): Boolean {
-        answeredCorrectly = true
+        play.answeredCorrectly = true
 
-        return answer()
+        return gameEngine.answer(play)
     }
 
     fun wrongAnswer(): Boolean {
-        answeredCorrectly = false
+        play.answeredCorrectly = false
 
-        return answer()
-    }
-
-    private fun play(roll: Roll) {
-        val currentPlayer = players.getCurrentPlayer()
-        playerRolledMessage(currentPlayer, roll)
-
-        if (currentPlayer.inPenaltyBox && roll.isEven()) {
-            stuckInPenaltyBoxMessage(currentPlayer)
-            isGettingOutOfPenaltyBox = false
-            return
-        }
-
-        if (currentPlayer.inPenaltyBox) {
-            gettingOutOfPenaltyBoxMessage(currentPlayer)
-            isGettingOutOfPenaltyBox = true
-        }
-
-        currentPlayer.move(roll)
-        playerMovedMessage(currentPlayer)
-        askQuestion()
-    }
-
-    private fun answer(): Boolean {
-        play(roll)
-
-        if (answeredCorrectly) {
-            val player = players.getCurrentPlayer()
-
-            if (player.inPenaltyBox && !isGettingOutOfPenaltyBox) {
-                players.nextPlayer()
-                return true
-            }
-
-            player.incrementScore()
-            playerAnsweredCorrectlyMessage(player)
-            players.nextPlayer()
-            return !player.isWinner()
-        } else {
-            playerAnsweredIncorrectlyMessage()
-            players.getCurrentPlayer().inPenaltyBox = true
-
-            players.nextPlayer()
-            return true
-        }
-    }
-
-    private fun askQuestion() {
-        println(currentCategory().takeCard())
-    }
-
-    private fun currentCategory(): QuestionCategory = when (players.getCurrentPlayer().place) {
-        0, 4, 8 -> popCategory
-        1, 5, 9 -> scienceCategory
-        2, 6, 10 -> sportsCategory
-        else -> rockCategory
-    }
-
-    private fun playerRolledMessage(currentPlayer: Player, roll: Roll) {
-        println(currentPlayer.name + " is the current player")
-        println("They have rolled a " + roll.value)
-    }
-
-    private fun playerAnsweredIncorrectlyMessage() {
-        println("Question was incorrectly answered")
-        println(players.getCurrentPlayer().name + " was sent to the penalty box")
-    }
-
-    private fun playerAnsweredCorrectlyMessage(player: Player) {
-        println("Answer was correct!!!!")
-        println("${player.name} now has ${player.purse} Gold Coins.")
-    }
-
-    private fun playerMovedMessage(currentPlayer: Player) {
-        println("${currentPlayer.name}'s new location is ${currentPlayer.place}")
-        println("The category is " + currentCategory().name)
-    }
-
-    private fun gettingOutOfPenaltyBoxMessage(currentPlayer: Player) {
-        println("${currentPlayer.name} is getting out of the penalty box")
-    }
-
-    private fun stuckInPenaltyBoxMessage(currentPlayer: Player) {
-        println("${currentPlayer.name} is not getting out of the penalty box")
+        return gameEngine.answer(play)
     }
 
     private fun playerAddedMessage(playerName: String) {
